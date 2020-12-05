e918898b0934f713080e8104c84ef7c349d38a50
Mateusz Tocha <m.tocha@tme3c.com>
2020-12-05 20:58:29 +0100

    Don't include dist folder

--- dist/THREEAR.d.ts
+++
@@ -1,8 +0,0 @@
-import Source from "./Source";
-import PatternMarker from "./PatternMarker";
-import BarcodeMarker from "./BarcodeMarker";
-import { Controller, ControllerParameters } from "./Controller";
-declare function initialize(
-	parameters: Partial<ControllerParameters>
-): Promise<any>;
-export { Controller, Source, initialize, PatternMarker, BarcodeMarker };
