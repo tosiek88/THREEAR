ad360cdeb3488c400b82a3270d153bf76734a74e
Mateusz Tocha <m.tocha@tme3c.com>
2020-12-05 20:59:13 +0100

    Export internal arToolkit

--- src/THREEAR.ts
+++ src/THREEAR.ts
@@ -2,13 +2,27 @@ import Source from "./Source";
 import PatternMarker from "./PatternMarker";
 import BarcodeMarker from "./BarcodeMarker";
 import { Controller, ControllerParameters } from "./Controller";
+import ARToolKit from "./artoolkit/ARToolKit";
+import ARToolKitCameraParam from "./artoolkit/ARToolKitCameraParam";
+import { ARToolKitController } from "./artoolkit/ARToolKitController";
 
 let controller: Controller;
-function initialize(parameters: Partial<ControllerParameters>) {
+function initialize(
+	parameters: Partial<ControllerParameters>
+): Promise<Controller> {
 	if (controller === undefined || controller.disposed === true) {
 		controller = new Controller(parameters);
 	}
-	return controller.postInit;
+	return controller.postInit as Promise<Controller>;
 }
 
-export { Controller, Source, initialize, PatternMarker, BarcodeMarker };
+export {
+	Controller,
+	Source,
+	initialize,
+	PatternMarker,
+	BarcodeMarker,
+	ARToolKit,
+	ARToolKitCameraParam,
+	ARToolKitController,
+};
